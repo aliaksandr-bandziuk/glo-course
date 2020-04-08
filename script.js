e9489ac1496306cd957a63050437c7d9f5aeff77
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-08 23:17:35 +0300

    no-commits

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 'use strict';
+
 // проверка на число
 let isNumber = function(n) {
     /*восклицательный знак значит "если не число, то вернется false" */
     /* isFinite - если конечное, то true, если бесконечное, то false */
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
+
+let start = function() {
+    money = +prompt('Ваш месячный доход?');
+    
+    while (!isNumber(money)) {
+        money = +prompt('Ваш месячный доход?');
+    }
+};
+
+start();
 
 let money,
  income = 'фриланс',
@@ -19,41 +30,50 @@ let money,
  accumulatedMonth;
 
 
- /* Вариатн цикла
-do {
-    console.log(mission);
-    mission++;
-}
-while(mission < 100000);
-*/
 
-let start = function() {
-    money = +prompt('Ваш месячный доход?');
-    
-    while (!isNumber(money)) {
-        money = +prompt('Ваш месячный доход?');
-    }
+let appData = {
+    budget: money,
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
 };
 
-start();
+//1) Функцию showTypeof и вызов функции удаляем 
+// function showTypeOf(data){
+//     return typeof data;
+// }
 
-let getStatusIncome = function(){
-    if (budgetDay >= 1200) {
-        return ('У вас высокий уровень дохода');
-    } else if ((budgetDay >= 600) || (budgetDay <= 1200)) {
-        return ('У вас средний уровень дохода');
-    } else if (budgetDay <= 600) {
-        return ('К сожалению, у вас уровень дохода ниже среднего');
-    } else if (budgetDay <= 0) {
-        return ('Что-то пошло не так');
-    }
+let getAccumulatedMonth = function(){
+    return money - expensesAmount;
 };
 
-function showTypeOf(data){
-    return typeof data;
-}
 
-//я не понимаю, для чего это
+budgetDay = accumulatedMonth / 30;
+
+
+
+//1) Функцию showTypeof и вызов функции удаляем 
+// console.log('money', showTypeOf(money));
+// console.log('income', showTypeOf(income));
+// console.log('deposit', showTypeOf(deposit));
+console.log('расходы за месяц: ', expensesAmount);
+console.log('возможные расходы: ', addExpenses.toLowerCase().split(', '));
+console.log('срок достижения цели: ', Math.ceil(getTargetMonth()));
+console.log('бюджет на день', budgetDay);
+console.log(getStatusIncome());
+
+
+
+
+// 2) В объект appData добавить свойство budget которое будет принимать значение money
+// 3) В объект appData добавить свойства budgetDay, budgetMonth и expensesMonth, изначально равные нулю
+
+
+
+// 4) Функции getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome - сделать методами объекта AppData
+
+
+let expensesAmount = appData.getExpensesMonth();
 let getExpensesMonth = function() {
     let sum = 0;
 
@@ -71,32 +91,30 @@ let getExpensesMonth = function() {
     return sum;
 };
 
-let expensesAmount = getExpensesMonth();
+accumulatedMonth = appData.getAccumulatedMonth();
 
-let getAccumulatedMonth = function(){
-    return money - expensesAmount;
-};
-
-accumulatedMonth = getAccumulatedMonth();
-budgetDay = accumulatedMonth / 30;
-
-function getTargetMonth(){
+function appData.getTargetMonth(){
     return mission / accumulatedMonth;
 }
 
-/*3) Если getTargetMonth возвращает нам отрицательное значение,
-то вместо “Цель будет достигнута” необходимо выводить “Цель не будет достигнута”*/
 if (getTargetMonth() >= 0){
     console.log('Цель будет достигнута');
 } else {
     console.log('Цель не будет достигнута');
 }
 
-console.log('money', showTypeOf(money));
-console.log('income', showTypeOf(income));
-console.log('deposit', showTypeOf(deposit));
-console.log('расходы за месяц: ', expensesAmount);
-console.log('возможные расходы: ', addExpenses.toLowerCase().split(', '));
-console.log('срок достижения цели: ', Math.ceil(getTargetMonth()));
-console.log('бюджет на день', budgetDay);
-console.log(getStatusIncome());
+let appData.getStatusIncome = function(){
+    if (budgetDay >= 1200) {
+        return ('У вас высокий уровень дохода');
+    } else if ((budgetDay >= 600) || (budgetDay <= 1200)) {
+        return ('У вас средний уровень дохода');
+    } else if (budgetDay <= 600) {
+        return ('К сожалению, у вас уровень дохода ниже среднего');
+    } else if (budgetDay <= 0) {
+        return ('Что-то пошло не так');
+    }
+};
+
+// 5) После этого поправить весь проект, чтобы он работал, а именно
+// Везде где вызывались наши функции поправить обращение через объект, например
+// let expensesMonth = appData.getExpensesMonth(); 
