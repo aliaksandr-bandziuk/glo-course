69827a44a04222080bd9f2f508a90be8f2fe83ec
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-09 16:31:37 +0300

    до 8 пункта все гуд

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let money;
 // проверка на число
 let isNumber = function(n) {
     /*восклицательный знак значит "если не число, то вернется false" */
@@ -17,93 +18,83 @@ let start = function() {
 
 start();
 
-let money,
- income = 'фриланс',
- addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
- deposit = confirm('Есть ли у вас депозит в банке?'),
- mission = 100000,
- period = 12,
- //6) budgetDay высчитываем исходя из значения месячного накопления (accumulatedMonth)
- budgetDay,
+let budgetDay,
  expenses1,
  expenses2,
  accumulatedMonth;
 
-
-
 let appData = {
+    // 2) В объект appData добавить свойство budget которое будет принимать значение money
+    // как вариант appData.budget: money, или budget {money: 'значение'},
     budget: money,
+    // 3) В объект appData добавить свойства budgetDay, budgetMonth и expensesMonth, изначально равные нулю
     budgetDay: 0,
     budgetMonth: 0,
     expensesMonth: 0,
-};
-
-//1) Функцию showTypeof и вызов функции удаляем 
-// function showTypeOf(data){
-//     return typeof data;
-// }
-
-let getAccumulatedMonth = function(){
-    return money - expensesAmount;
-};
-
-
-budgetDay = accumulatedMonth / 30;
-
-
-
-//1) Функцию showTypeof и вызов функции удаляем 
-// console.log('money', showTypeOf(money));
-// console.log('income', showTypeOf(income));
-// console.log('deposit', showTypeOf(deposit));
-console.log('расходы за месяц: ', expensesAmount);
-console.log('возможные расходы: ', addExpenses.toLowerCase().split(', '));
-console.log('срок достижения цели: ', Math.ceil(getTargetMonth()));
-console.log('бюджет на день', budgetDay);
-console.log(getStatusIncome());
-
-
-
-
-// 2) В объект appData добавить свойство budget которое будет принимать значение money
-// 3) В объект appData добавить свойства budgetDay, budgetMonth и expensesMonth, изначально равные нулю
-
-
-
-// 4) Функции getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome - сделать методами объекта AppData
-
-
-let expensesAmount = appData.getExpensesMonth();
-let getExpensesMonth = function() {
-    let sum = 0;
-
-    for (let i = 0; i < 2; i++) {
-
-        if (i === 0) {
-            expenses1 = +prompt('Введите обязательную статью расходов');
-        } else if (i === 1) {
-            expenses2 = +prompt('Введите еще одну обязательную статью расходов');
+    // 4) Функции getExpensesMonth, getAccumulatedMonth, getTargetMonth, getStatusIncome
+    //сделать методами объекта AppData
+    getExpensesMonth: function() {
+        // let sum = 0;
+        // for (let i = 0; i < 2; i++) {
+        //     if (i === 0) {
+        //         expenses1 = +prompt('Введите обязательную статью расходов');
+        //     } else if (i === 1) {
+        //         expenses2 = +prompt('Введите еще одну обязательную статью расходов');
+        //     }
+        //     sum += +prompt('Во сколько это обойдется?');
+        // }
+        // console.log(sum);
+        // return sum;
+    },
+    getAccumulatedMonth: function(){
+        return money - expensesAmount;
+    },
+    getTargetMonth: {},
+    getStatusIncome: {},
+    income: {},
+    addIncome: [],
+    // 7) ... чтобы результат записывался в объект  appData.expenses
+    expenses: {
+        expenses1: [],
+        expenses2: [],
+    },
+    addExpenses: [],
+    deposit: false,
+    mission: 100000,
+    period: 12,
+    asking: function(){
+        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+        appData.addExpenses = addExpenses.toLowerCase().split(', ');
+        appData.deposit = confirm('Есть ли у вас депозит в банке?');
+        // 7) Перенести цикл из метода getExpensesMonth в метод asking, и переписать цикл таким образом 
+        // чтобы результат записывался в объект  appData.expenses
+        let sum = 0;
+        for (let i = 0; i < 2; i++) {
+            if (i === 0) {
+                expenses1 = +prompt('Введите обязательную статью расходов');
+            } else if (i === 1) {
+                expenses2 = +prompt('Введите еще одну обязательную статью расходов');
+            }
+            sum += +prompt('Во сколько это обойдется?');
         }
-
-        sum += +prompt('Во сколько это обойдется?');
+        console.log(sum);
+        return sum;
     }
-    console.log(sum);
-    return sum;
 };
+// 6) Сразу после объекта выполните вызов
+appData.asking();
 
-accumulatedMonth = appData.getAccumulatedMonth();
 
-function appData.getTargetMonth(){
-    return mission / accumulatedMonth;
-}
 
-if (getTargetMonth() >= 0){
-    console.log('Цель будет достигнута');
-} else {
-    console.log('Цель не будет достигнута');
-}
+// 5) После этого поправить весь проект, чтобы он работал, а именно
+// Везде где вызывались наши функции поправить обращение через объект, например
+// let expensesMonth = appData.getExpensesMonth(); 
 
-let appData.getStatusIncome = function(){
+
+
+
+
+let getStatusIncome = function(){
     if (budgetDay >= 1200) {
         return ('У вас высокий уровень дохода');
     } else if ((budgetDay >= 600) || (budgetDay <= 1200)) {
@@ -115,6 +106,31 @@ let appData.getStatusIncome = function(){
     }
 };
 
-// 5) После этого поправить весь проект, чтобы он работал, а именно
-// Везде где вызывались наши функции поправить обращение через объект, например
-// let expensesMonth = appData.getExpensesMonth(); 
+// 1) Функцию showTypeof и вызов функции удаляем 
+// function showTypeOf(data){
+//     return typeof data;
+// }
+
+let expensesAmount = appData.getExpensesMonth();
+
+accumulatedMonth = appData.getAccumulatedMonth();
+budgetDay = accumulatedMonth / 30;
+
+function getTargetMonth(){
+    return appData.mission / accumulatedMonth;
+}
+
+if (getTargetMonth() >= 0){
+    console.log('Цель будет достигнута');
+} else {
+    console.log('Цель не будет достигнута');
+}
+
+// console.log('money', showTypeOf(money));
+// console.log('income', showTypeOf(appData.income));
+// console.log('deposit', showTypeOf(appData.deposit));
+console.log('расходы за месяц: ', expensesAmount);
+
+console.log('срок достижения цели: ', Math.ceil(getTargetMonth()));
+console.log('бюджет на день', budgetDay);
+console.log(getStatusIncome());
