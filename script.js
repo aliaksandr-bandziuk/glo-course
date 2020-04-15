05ef3853642dac4821d4d50238bc6476d8bce146
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-15 14:44:54 +0300

    update script

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 'use strict';
 
-const books = document.querySelectorAll('.book'),
+const books = document.querySelectorAll('.books'),
+    book = document.querySelectorAll('.book'),
+    bodyDoc = document.querySelector('body'),
+    liTwo = book[0].querySelectorAll('li'),
+    liFive = book[5].querySelectorAll('li'),
     bannerAdv = document.querySelector('.adv'),
-    newChapter = document.createElement('li'),
-    bodyDoc = document.querySelector('body');
+    newChapter = document.createElement('li');
+    
+    // Восстановить порядок книг.
 
-// Восстановить порядок книг.
-
-books[0].before(books[1]);
-books[5].after(books[2]);
-books[5].before(books[3]);
+book[0].before(book[1]);
+book[5].after(book[2]);
+book[5].before(book[3]);
 
 // Заменить картинку заднего фона на другую из папки image
 bodyDoc.style.backgroundImage = 'url(./image/adv.jpg)';
 
 // Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
-books[3].querySelector.textContent = 'Книга 3. this и Прототипы Объектов';
+book[3].querySelector.textContent = 'Книга 3. this и Прототипы Объектов';
 
 // Удалить рекламу со страницы
 bannerAdv.remove();
 
+// Порядок во второй главе
+liTwo[7].after(liTwo[2]);
+liTwo[3].after(liTwo[6]);
+liTwo[6].after(liTwo[8]);
+liTwo[9].before(liTwo[7]);
+liTwo[9].after(liTwo[2]);
 
-// Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
+// Порядок в пятой главе
+liFive[1].after(liFive[9]);
+liFive[4].after(liFive[2]);
+liFive[8].before(liFive[5]);
 
 // в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
-books[2].append('newChapter');
+book[2].append('newChapter');
 newChapter.textContent = 'Глава 8: За пределами ES6';
